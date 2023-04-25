d338f366ee1fc7539f23e6f5afb0611c62a39aff
LaamaHaddad <laama.haddad.it@gmail.com>
2023-04-25 19:07:14 +0400

    Change app's name

--- src/config.tsx
+++ src/config.tsx
@@ -2,8 +2,8 @@ import {Config} from './resources/interfaces/config';
 
 const config: Config = {
   debug: true,
-  APP_NAME: 'BlueB2S',
-  KEY_NAME: 'blueB2S',
+  APP_NAME: 'Be Shine',
+  KEY_NAME: 'beShine',
   GOOGLE_API_KEY: 'AIzaSyCYI-IHNy_X06nKvvAOgLGXjuYqaVkGGas',
 };
 
